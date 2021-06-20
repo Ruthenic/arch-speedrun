47354c35049ab16420015b6beba9d02e34235626
z4221 <z4221@users.noreply.github.com>
2021-06-20 13:21:24 +0000

    deploy: 115f08d1eeaebc329b6226f92e739cfec0e1556a

--- footer.js
+++ footer.js
@@ -3,7 +3,7 @@ async function getFooter(footerPath) {
 		//why the hell does js not have optional parameters for functions? 
 		footerPath = "/footer.html";
 	}
-	console.log("sanity check much?");
+	console.log("Ey lad hope your enjoying the website");
 	console.log(footerPath)
 	footer = await fetch(footerPath);
 	footer = await footer.text();
